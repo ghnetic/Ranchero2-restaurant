74da59e7a9fdbdbc744291b5d3ee181b5ac333ff
Gineth <sofiagineth4@gmail.com>
2024-06-14 14:59:05 -0400

    Cambios en la web
    Cambios

--- js/theme.js
+++ js/theme.js
@@ -258,7 +258,6 @@ window.theme.fn = {
 	},
 
 	showErrorMessage: function(title, content) {
-
 		$('.modalThemeErrorMessage').remove();
 		$('body').append('<div class="modal fade" id="modalThemeErrorMessage" aria-hidden="true" tabindex="-1"><div class="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + title + '</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">' + content + '</div><div class="modal-footer"><button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button></div></div></div></div>');
 
